6233873bfd307e3cea420ef3ae48bef0ce2ddb5f
hannibalboom <2179928851@qq.com>
2024-01-17 15:20:26 +0800

    first commit

---
+++ 1.16-PrintStudy/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿
+namespace _1._16_PrintStudy
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_Print = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.btn_uploadImages = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn_Print
+            // 
+            this.btn_Print.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.btn_Print.Font = new System.Drawing.Font("微软雅黑", 10.28571F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btn_Print.Location = new System.Drawing.Point(182, 317);
+            this.btn_Print.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btn_Print.Name = "btn_Print";
+            this.btn_Print.Size = new System.Drawing.Size(63, 30);
+            this.btn_Print.TabIndex = 1;
+            this.btn_Print.Text = "Print";
+            this.btn_Print.UseVisualStyleBackColor = false;
+            this.btn_Print.Click += new System.EventHandler(this.btn_Print_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(57, 39);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(149, 107);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(222, 39);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(149, 107);
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(57, 161);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(149, 107);
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Location = new System.Drawing.Point(222, 161);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(149, 107);
+            this.pictureBox4.TabIndex = 2;
+            this.pictureBox4.TabStop = false;
+            // 
+            // btn_uploadImages
+            // 
+            this.btn_uploadImages.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.btn_uploadImages.Font = new System.Drawing.Font("微软雅黑", 10.28571F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btn_uploadImages.Location = new System.Drawing.Point(182, 283);
+            this.btn_uploadImages.Margin = new System.Windows.Forms.Padding(2);
+            this.btn_uploadImages.Name = "btn_uploadImages";
+            this.btn_uploadImages.Size = new System.Drawing.Size(63, 30);
+            this.btn_uploadImages.TabIndex = 1;
+            this.btn_uploadImages.Text = "Upload Images";
+            this.btn_uploadImages.UseVisualStyleBackColor = false;
+            this.btn_uploadImages.Click += new System.EventHandler(this.btn_UploadImage_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(422, 355);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btn_uploadImages);
+            this.Controls.Add(this.btn_Print);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_Print;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.Button btn_uploadImages;
+    }
+}
+
---
+++ 1.16-PrintStudy/Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _1._16_PrintStudy
+{
+    public partial class Form1 : Form
+    {
+        public PrintFrm Printer { get; set; }//声明打印机
+        public List<PictureBox> pictureboxes;
+        public List<Image> uploadedImages;
+        public List<string> selectedImagePaths;
+        public Form1()
+        {
+            InitializeComponent();
+            //用于图片预览
+            pictureboxes = new List<PictureBox> { pictureBox1, pictureBox2, pictureBox3, pictureBox4 };
+            //存放图片
+            uploadedImages = new List<Image>();
+            selectedImagePaths = new List<string>();
+        }
+
+        private void btn_Print_Click(object sender, EventArgs e)
+        {
+            //假定一个对象
+            String textToPrint = "Hello, this is a dynamic print object.";
+            PrintFrm frm = new PrintFrm(uploadedImages);
+            if (Printer == null || Printer.IsDisposed)//为空或已被释放
+            {
+                Printer = new PrintFrm(uploadedImages);//实例化
+                Printer.ShowInTaskbar = false;//窗体不会在任务栏中显示
+                Printer.StartPosition = FormStartPosition.CenterScreen;//窗体起始位置为屏幕中央
+                Printer.ShowDialog();//此时用户只能与该窗体交互
+            }
+            else
+            {
+                Printer.Activate();//已经存在，则将窗体置于前台
+            }
+            //初始化个人信息
+            string patientName = "张三";
+            string patientSex = "男";
+            string patientAge = "20";
+            string patientId = "20231114117100";
+            string patientR_eye = "";
+            string patientL_eye = "";
+            frm.uploadedImages = uploadedImages;
+            frm.selectedImagePaths = selectedImagePaths;
+            //传递个人信息
+            frm.patientName = patientName;
+            frm.patientSex = patientSex;
+            frm.patientAge = patientAge;
+            frm.patientId = patientId;
+            frm.patientR_eye = patientR_eye;
+            frm.patientL_eye = patientL_eye;
+            frm.Show();
+        }
+        private void btn_UploadImage_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog
+            {
+                //运行用户选择多个文件
+                Multiselect = true,
+                Filter = "Image Files|*.jpg;*.jpeg;*.png;*.gif;*.bmp|All Files|*.*"
+            };
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                // 获取用户选择的所有图片文件路径
+                selectedImagePaths.AddRange(openFileDialog.FileNames);
+                string[] selectedImage = openFileDialog.FileNames;
+
+                // 显示图片
+                ShowImages(selectedImage);
+            }
+        }
+        private void ShowImages(string[] imagePaths)
+        {
+            foreach (string imagePath in imagePaths)
+            {
+                //添加新上传的图片到列表
+                Image newImage = Image.FromFile(imagePath);
+                uploadedImages.Add(newImage);
+                // 检查图片数量是否超过4，如果是，移除第一个元素
+                while (uploadedImages.Count > 4)
+                {
+                    uploadedImages.RemoveAt(0);
+                }
+                // 遍历 PictureBox 数组，显示所有上传的图片
+                for (int i = 0; i < pictureboxes.Count; i++)
+                {
+                    // 如果有足够的上传图片，则设置 PictureBox 的 Image 属性
+                    if (i < uploadedImages.Count)
+                    {
+                        pictureboxes[i].Image = uploadedImages[i];
+                    }
+                    else
+                    {
+                        // 如果上传图片不足，可以清空 PictureBox
+                        pictureboxes[i].Image = null;
+                    }
+                }
+            }
+        }
+    }
+}
